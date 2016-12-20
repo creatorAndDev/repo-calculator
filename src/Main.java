86ca91580b1db9dca1cb9f5ec71323694bb7d68d
Yuri <creator.and.dev@gmail.com>
2016-12-20 15:22:18 +0200

    Рабочая версия калькулятора

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-import java.awt.*;
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class Main extends JFrame{
 	
@@ -7,11 +14,15 @@ public class Main extends JFrame{
 	private int WIDTH = 250;
 	private int HEIGHT = 350;
 	
+	private int firstValue = 0;
+	private String operation = "+";
+	
 	//Содержимое окна
 	JTextArea display = new JTextArea(); //дисплей калькулятора
 	
 	JPanel buttonPanel = new JPanel(new GridLayout(3,5)); //панель с кнопками
 	
+	//создаем обьекты кнопок
     JButton button0 = new JButton("0"); //кнопка 0
     JButton button1 = new JButton("1"); //кнопка 1
     JButton button2 = new JButton("2"); //кнопка 2
@@ -43,7 +54,7 @@ public class Main extends JFrame{
 		add(buttonPanel, BorderLayout.CENTER); //добавляем область для кнопок от 0 - 9 +=-/...
         add(buttonStart, BorderLayout.SOUTH); //добавляем кнопку =
         
-        //добавляем кнопки
+        //добавляем кнопки на панель buttonPanel
         buttonPanel.add(button0);
         buttonPanel.add(button1);
         buttonPanel.add(button2);
@@ -60,11 +71,165 @@ public class Main extends JFrame{
         buttonPanel.add(buttonDivide);
         buttonPanel.add(buttonSub);
         buttonPanel.add(buttonMul);
+        
+        //действия при клике
+        //Добавление цифр в область textarea
+        //слушатели
+        button0.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                display.setText(display.getText() + "0");
+            }
+        });
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                display.setText(display.getText() + "1");
+            }
+        });
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                display.setText(display.getText() + "2");
+            }
+        });
+        button3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                display.setText(display.getText() + "3");
+            }
+        });
+        button4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                display.setText(display.getText() + "4");
+            }
+        });
+        button5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                display.setText(display.getText() + "5");
+            }
+        });
+        button6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                display.setText(display.getText() + "6");
+            }
+        });
+        button7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                display.setText(display.getText() + "7");
+            }
+        });
+        button8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                display.setText(display.getText() + "8");
+            }
+        });
+        button9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                display.setText(display.getText() + "9");
+            }
+        });
+        
+        //кнопка "C" будет удалять
+        buttonBack.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String temp = display.getText();
+                display.setText(temp.substring(0, temp.length() - 1));
+            }
+        });
+        
+        //действия выполнения операции над цифрами
+        //1) очищаем поле при действии операции над цифрами * + - /
+        buttonSum.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                firstValue = Integer.valueOf(display.getText());
+                display.setText("");
+                operation = "+";
+            }
+        });
+        buttonMul.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                firstValue = Integer.valueOf(display.getText());
+                display.setText("");
+                operation = "*";
+            }
+        });
+        buttonDivide.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                firstValue = Integer.valueOf(display.getText());
+                display.setText("");
+                operation = "/";
+            }
+        });
+        buttonSub.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                firstValue = Integer.valueOf(display.getText());
+                display.setText("");
+                operation = "-";
+            }
+        });
+        
+        //2) реализация =
+//        try{
+//        	buttonStart.addActionListener(new ActionListener() {
+//                @Override
+//                public void actionPerformed(ActionEvent e) {
+//                    int secondValue = Integer.valueOf(display.getText());
+//                    if("+".equals(operation)){
+//                        display.setText((firstValue + secondValue) + "");
+//                    }
+//                    if("-".equals(operation)){
+//                        display.setText((firstValue - secondValue) + "");
+//                    }
+//                    if("*".equals(operation)){
+//                        display.setText((firstValue * secondValue) + "");
+//                    }
+//                    if("/".equals(operation)){
+//                        display.setText((firstValue / secondValue) + "");
+//                    }
+//                    firstValue = 0;
+//                    operation = "+";
+//                }
+//            });
+//        } catch(NumberFormatException e){
+//        	System.out.println("Пусто!");
+//        }
+        buttonStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int secondValue = Integer.valueOf(display.getText());
+                if("+".equals(operation)){
+                    display.setText((firstValue + secondValue) + "");
+                }
+                if("-".equals(operation)){
+                    display.setText((firstValue - secondValue) + "");
+                }
+                if("*".equals(operation)){
+                    display.setText((firstValue * secondValue) + "");
+                }
+                if("/".equals(operation)){
+                    display.setText((firstValue / secondValue) + "");
+                }
+                firstValue = 0;
+                operation = "+";
+            }
+        });
 	}
 	
 	//MAIN
 	public static void main(String[] args) {
-		Main app = new Main(); //создаем окно на основе нашего класса
+		Main app = new Main(); //создаем окно калькулятора на основе нашего класса
 		app.setVisible(true); //доступ true - что бы был виден
 		app.setLocationRelativeTo(null); //позиция окна
 	}
