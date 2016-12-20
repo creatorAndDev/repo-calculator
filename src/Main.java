a1af3f602c8f3d30e3f91eb5e4f96a21a1d76979
Yuri <creator.and.dev@gmail.com>
2016-12-20 14:11:08 +0200

    Создал окно

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
+import javax.swing.JFrame;
 
-public class Main {
+public class Main extends JFrame{
+	
+	//fields
+	int WIDTH = 250;
+	int HEIGHT = 350;
 
+	//constructor main for jframe
+	public Main(){
+		super("Калькулятор"); //заголовок окна
+		
+		setResizable(false); //запрещаем растягивать окно
+		setSize(WIDTH, HEIGHT); //размер окна width, height
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // закрытие окна по крестику
+	}
+	
+	//main
 	public static void main(String[] args) {
-//		System.out.println("Hello world");
+		Main app = new Main(); //создаем окно на основе нашего класса
+		app.setVisible(true); //доступ true - что бы был виден
+		app.setLocationRelativeTo(null); //позиция окна
 	}
 
 }
